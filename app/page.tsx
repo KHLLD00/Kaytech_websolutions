9fd43d96da3a25213520a41419473aad838ea3f1
KHLLD00 <usmankhaleed899@gmail.com>
2026-09-24 15:34:18 +0100

    Add testimonials to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/sections/HeroSection";
 import IntroSection from "@/components/sections/IntroSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
+import TestimonialsSection from "@/components/testimonials/TestimonialsSection";
 import WhyKaytechSection from "@/components/sections/WhyKaytechSection";
 import PricingSection from "@/components/sections/PricingSection";
 import AddOnsSection from "@/components/sections/AddOnsSection";
@@ -17,6 +18,7 @@ export default function Home() {
       <IntroSection />
       <ServicesSection />
       <ProjectsSection />
+      <TestimonialsSection />
       <WhyKaytechSection />
       <PricingSection />
       <AddOnsSection />
